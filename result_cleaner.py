6b3cca5eb43798fe35a137f590eb1ae82eed96a1
Milindi_Kodikara <milindikodikara@gmail.com>
2024-08-25 17:59:17 +1000

    Fix hallucinations for ner, re

--- result_cleaner.py
+++ result_cleaner.py
@@ -42,7 +42,7 @@ def extract_entities(results, task, annotation):
     if task == 'RE':
         annotation_pattern = '^(?P<span1>.+)[\t](?P<span2>.+)[\t](?P<relation_type>' + split_annotations + ')$'
         extracted_entities = pd.DataFrame(
-            columns=['pmid', 'mark1', 'label1', 'offset1_start', 'offset1_end', 'span1',
+            columns=['pmid', 'prompt_id', 'offsets_checked', 'mark1', 'label1', 'offset1_start', 'offset1_end', 'span1',
                      'mark2', 'label2', 'offset2_start', 'offset2_end', 'span2',
                      'relation_mark', 'relation_type'])
 
@@ -74,73 +74,118 @@ def extract_entities(results, task, annotation):
                         }
                     elif task == 'RE':
                         df_row = {
-                            'pmid': pmid,
-                            'mark1': 'T' + str(len(extracted_entities)) + '_gene',
-                            'label1': 'Gene',
-                            'offset1_start': '',
-                            'offset1_end': '',
-                            'span1': extracted_item['span1'],
-                            'mark2': 'T' + str(len(extracted_entities)) + '_disease',
-                            'label2': 'Disease',
-                            'offset2_start': '',
-                            'offset2_end': '',
-                            'span2': extracted_item['span2'],
-                            'relation_mark': 'R' + str(len(extracted_entities)),
-                            'relation_type': extracted_item['relation_type']
+                            "pmid": pmid,
+                            "prompt_id": prompt_id,
+                            "offsets_checked": False,
+                            "mark1": 'T' + str(len(extracted_entities)) + '_gene',
+                            "label1": 'Gene',
+                            "offset1_start": '',
+                            "offset1_end": '',
+                            "span1": extracted_item['span1'],
+                            "mark2": 'T' + str(len(extracted_entities)) + '_disease',
+                            "label2": 'Disease',
+                            "offset2_start": '',
+                            "offset2_end": '',
+                            "span2": extracted_item['span2'],
+                            "relation_mark": 'R' + str(len(extracted_entities)),
+                            "relation_type": extracted_item['relation_type']
                         }
                     extracted_entities.loc[len(extracted_entities)] = df_row
 
     return extracted_entities
 
 
-# TODO: Fix hallucinations
-def get_hallucinations(text_df, extracted_entity_df):
-    # loop df, find each span, calculate the word length, find the indexes of each occurance
-    for _, row in extracted_entity_df.iterrows():
+def mark_hallucinated_fabricated_spans(row_index, row_text, row, extracted_entity_df, task):
+    if task == "NER":
+        span = row["span"]
+
+        if not row['offset_checked'] and span not in row_text:
+            extracted_entity_df.loc[row_index, 'offset_checked'] = True
+            extracted_entity_df.loc[row_index, 'offset1'] = '-1'
+            extracted_entity_df.loc[row_index, 'offset2'] = '-1'
+
+    if task == 'RE':
+        span1 = row['span1']
+        span2 = row['span2']
+
+        if not row['offsets_checked'] and not span1 in row_text and not span2 in row_text:
+            extracted_entity_df.loc[row_index, 'offsets_checked'] = True
+
+            extracted_entity_df.loc[row_index, 'offset1_start'] = '-1'
+            extracted_entity_df.loc[row_index, 'offset1_end'] = '-1'
+
+            extracted_entity_df.loc[row_index, 'offset2_start'] = '-1'
+            extracted_entity_df.loc[row_index, 'offset2_end'] = '-1'
+
+
+def mark_hallucinated_extra_spans(row_text, row, extracted_entity_df, prompt_id):
+    if not row['offset_checked']:
+        span = row['span']
+        span_length = len(span)
+
+        # span occurrences extracted
+        matching_spans = extracted_entity_df[
+            (extracted_entity_df['pmid'] == row['pmid']) & (extracted_entity_df['prompt_id'] == prompt_id) & (
+                    extracted_entity_df['span'] == span)]
+
+        # start indexes of the all the mentions of the span from the text
+        span_start_indexes = [m.start() for m in re.finditer(re.escape(span), row_text)]
+        # number of occurrences of the span in the text
+        span_occurrences_in_text = len(span_start_indexes)
+
+        span_count = 0
+
+        for index, matched_span in matching_spans.iterrows():
+            if not matched_span['offset_checked']:
+                if span_count < span_occurrences_in_text:
+                    extracted_entity_df.loc[index, 'offset1'] = str(span_start_indexes[span_count])
+                    extracted_entity_df.loc[index, 'offset2'] = str(span_start_indexes[span_count] + span_length)
+                    span_count = span_count + 1
+            else:
+                # this means that the span is an extra
+                extracted_entity_df.loc[index, 'offset1'] = '-2'
+                extracted_entity_df.loc[index, 'offset2'] = '-2'
+
+            extracted_entity_df.loc[index, 'offset_checked'] = True
+
+
+def get_hallucinations(text_df, extracted_entity_df, task):
+    # loop df, find each span, calculate the word length, find the indexes of each occurrence
+    correct_entities = extracted_entity_df
+    hallucinated_results = pd.DataFrame()
+
+    for row_index, row in extracted_entity_df.iterrows():
         pmid = row['pmid']
         prompt_id = row['prompt_id']
         # find the text from the original_data with the pmid
-        text = text_df.loc[text_df['pmid'] == pmid, 'text'].iloc[0]
+        row_text = text_df.loc[text_df['pmid'] == pmid, 'text'].iloc[0]
 
-        if not row['offset_checked'] and row['offset1'] == '':
-            span = row['span']
-            span_length = len(span)
-            span_start_indexes = [m.start() for m in re.finditer(re.escape(span), text)]
-            span_count = 0
+        mark_hallucinated_fabricated_spans(row_index, row_text, row, extracted_entity_df, task)
 
-            matching_spans = extracted_entity_df[
-                (extracted_entity_df['pmid'] == pmid) & (extracted_entity_df['prompt_id'] == prompt_id) & (
-                        extracted_entity_df['span'] == span) & (extracted_entity_df['offset1'] == '') & (
-                        extracted_entity_df['offset_checked'] == False)]
+        if task == 'NER':
+            mark_hallucinated_extra_spans(row_text, row, extracted_entity_df, prompt_id)
 
-            for index, matched_span in matching_spans.iterrows():
-                if span_start_indexes and span_count < len(span_start_indexes):
-                    extracted_entity_df.loc[index, 'offset1'] = str(span_start_indexes[span_count])
-                    extracted_entity_df.loc[index, 'offset2'] = str(span_start_indexes[span_count] + span_length)
+            hallucinated_results = extracted_entity_df[
+                (extracted_entity_df['offset1'] == '-1') & (extracted_entity_df['offset2'] == '-1') &
+                (extracted_entity_df['offset1'] == '-2') & (extracted_entity_df['offset2'] == '-2')]
 
-                    span_count = span_count + 1
-                else:
-                    # Add -1 to extra or missing ones
-                    extracted_entity_df.loc[index, 'offset1'] = '-1'
-                    extracted_entity_df.loc[index, 'offset2'] = '-1'
+            correct_entities = extracted_entity_df[
+                (extracted_entity_df['offset1'] != '-1') & (extracted_entity_df['offset2'] != '-1') &
+                (extracted_entity_df['offset1'] != '-2') & (extracted_entity_df['offset2'] != '-2')]
+        elif task == 'RE':
+            hallucinated_results = extracted_entity_df[
+                (extracted_entity_df['offset1_start'] == '-1') & (extracted_entity_df['offset2_start'] == '-1')]
 
-                extracted_entity_df.loc[index, 'offset_checked'] = True
+            correct_entities = extracted_entity_df[
+                (extracted_entity_df['offset1_start'] != '-1') & (extracted_entity_df['offset2_start'] != '-1')]
 
-    hallucinated_results = extracted_entity_df[
-        (extracted_entity_df['offset1'] == '-1') & (extracted_entity_df['offset2'] == '-1')]
-
-    return hallucinated_results
+    return correct_entities, hallucinated_results
 
 
-# TODO: Refactor this file, it is a mess, girl!
 def result_cleaner(text, results, annotation, task):
     extracted_entities = extract_entities(results, task, annotation)
 
     print(f"Results len: {len(results)}\n{results}\n\n")
     print(f"Extracted entities len: {len(extracted_entities)}\n{extracted_entities.head().to_string()}\n\n")
 
-    hallucinated_entities = get_hallucinations(text, extracted_entities)
-    correct_entities = extracted_entities[
-        (extracted_entities['offset1'] != '-1') & (extracted_entities['offset2'] != '-1')]
-
-    return correct_entities, hallucinated_entities
+    return get_hallucinations(text, extracted_entities, task)
