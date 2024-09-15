8defa8d5172322f787a53258d8996ff7f95c5e82
Milindi_Kodikara <milindikodikara@gmail.com>
2024-09-15 13:15:35 +1000

    Set up nb

--- analysis.py
+++ analysis.py
@@ -4,25 +4,33 @@ import nbformat as nbf
 
 
 def generate_jupyter_notebook(nb, filepath):
-    file_name = f'{filepath}.ipynb'
+    file_name = f'{filepath}/analysis.ipynb'
     with open(file_name, 'w') as f:
         nbf.write(nb, f)
 # TODO: Automatically execute the nb
 
 
-def analysis(cleaned_entities, hallucinations, evaluation_values, filepath):
+def add_cell(nb, cell_data, cell_type="code"):
+    if cell_type == "mk":
+        nb['cells'].append(nbf.v4.new_markdown_cell(cell_data))
+        return nb
+    elif cell_type == "code":
+        nb['cells'].append(nbf.v4.new_code_cell(cell_data))
+        return nb
+
+
+def analysis(filepath):
     nb = nbf.v4.new_notebook()
     nb['cells'] = []
 
     intro = """#### RMIT READ-BioMed-Version-2.0\n### Milindi Kodikara"""
-    nb['cells'].append(nbf.v4.new_markdown_cell(intro))
+    nb = add_cell(nb, intro, "mk")
 
     imports = """import pandas as pd\nimport numpy as np\nimport matplotlib.pyplot as plt
     """
-    nb['cells'].append(nbf.v4.new_code_cell(imports))
+    nb = add_cell(nb, imports)
 
-    code = """%pylab inline\nhist(normal(size=2000), bins=50);"""
-
-    nb['cells'].append(nbf.v4.new_code_cell(code))
+    setup = """evaluation_df = pd.read_csv('./eval_log.tsv', sep='\t', header=0)\nevaluation_df.head()"""
+    nb = add_cell(nb, setup)
 
     generate_jupyter_notebook(nb, filepath)
